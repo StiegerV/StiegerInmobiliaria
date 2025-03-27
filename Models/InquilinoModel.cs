e781cfd47964fefcab3f0e74ccf167a32691a9e2
Valentin Stieger <50178370+StiegerV@users.noreply.github.com>
2025-03-27 20:19:41 -0300

    crud inquilino

--- Models/InquilinoModel.cs
+++ Models/InquilinoModel.cs
@@ -3,13 +3,29 @@ using Name;
 
 namespace StiegerModels
 {
-    class InquilinoModel:personaModel
+    class InquilinoModel : personaModel
     {
         [Key]
         public int Id_inquilino { get; set; }
 
 
+        public InquilinoModel() { }
+
+        public InquilinoModel(string dni, string nombre, string apellido, string contacto)
+        {
+            this.Dni = dni;
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Contacto = contacto;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Nombre} {Apellido}, dni: {Dni}";
+        }
     }
+
 
 
 }
---
+++ Models/RepositorioInquilino.cs
@@ -0,0 +1,121 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace StiegerModels
+{
+    class RepositorioInquilino : Conexion, IrepositorioInquilino
+    {
+        public int Alta(InquilinoModel i)
+        {
+            this.abrirConexion();
+            int id = -1;
+            string sql = @"INSERT INTO `inquilino`(`dni`, `nombre`, `apellido`, `contacto`)
+                 VALUES (@dni,@nombre,@apellido,@contacto);
+               SELECT LAST_INSERT_ID();";
+
+            MySqlCommand comando = new MySqlCommand(sql, this.conexionsql);
+            comando.Parameters.AddWithValue("@dni", i.Dni);
+            comando.Parameters.AddWithValue("@nombre", i.Nombre);
+            comando.Parameters.AddWithValue("@apellido", i.Apellido);
+            comando.Parameters.AddWithValue("@contacto", i.Contacto);
+            comando.CommandType = CommandType.Text;
+            id = Convert.ToInt32(comando.ExecuteScalar());
+            i.Id_inquilino = id;
+            this.cerrarConexion();
+
+            return id;
+        }
+
+        public int Baja(InquilinoModel i)
+        {
+            this.abrirConexion();
+            int columnasAfectadas = -1;
+            string sql = @"DELETE FROM `inquilino` WHERE `id_inquilino`=@id;";
+
+            MySqlCommand comando = new MySqlCommand(sql, this.conexionsql);
+            comando.Parameters.AddWithValue("@id", i.Id_inquilino);
+            comando.CommandType = CommandType.Text;
+            columnasAfectadas = Convert.ToInt32(comando.ExecuteNonQuery());
+
+            this.cerrarConexion();
+
+            return columnasAfectadas;
+
+        }
+
+
+        public int Modificacion(InquilinoModel i)
+        {
+            this.abrirConexion();
+            int columnasAfectadas = -1;
+            string sql = @"UPDATE `inquilino` SET`dni`=@dni,`nombre`=@nombre,`apellido`=@apellido,`contacto`=@contacto
+                            WHERE `id_inquilino`=@id";
+
+            MySqlCommand comando = new MySqlCommand(sql, this.conexionsql);
+            comando.Parameters.AddWithValue("@dni", i.Dni);
+            comando.Parameters.AddWithValue("@nombre", i.Nombre);
+            comando.Parameters.AddWithValue("@apellido", i.Apellido);
+            comando.Parameters.AddWithValue("@contacto", i.Contacto);
+            comando.Parameters.AddWithValue("@id", i.Id_inquilino);
+            comando.CommandType = CommandType.Text;
+            columnasAfectadas = Convert.ToInt32(comando.ExecuteNonQuery());
+
+            this.cerrarConexion();
+
+            return columnasAfectadas;
+        }
+
+        public List<InquilinoModel> TraerTodos()
+        {
+            List<InquilinoModel> inquilinos = new List<InquilinoModel>();
+
+            this.abrirConexion();
+            string sql = @"SELECT * FROM inquilino";
+            MySqlCommand comando = new MySqlCommand(sql, this.conexionsql);
+            comando.CommandType = CommandType.Text;
+            var lector = comando.ExecuteReader();
+            while (lector.Read())
+            {
+                InquilinoModel i = new InquilinoModel();
+                i.Id_inquilino = lector.GetInt16("id_inquilino");
+                i.Dni = lector.GetString("dni");
+                i.Nombre = lector.GetString("nombre");
+                i.Apellido = lector.GetString("apellido");
+                i.Contacto = lector.GetString("contacto");
+
+                inquilinos.Add(i);
+            }
+            this.cerrarConexion();
+
+
+            return inquilinos;
+        }
+
+        //SELECT `id_inquilino`, `dni`, `nombre`, `apellido`, `contacto` FROM `inquilino` WHERE `id_inquilino`=@id
+        public InquilinoModel TraerId(int id)
+        {
+            this.abrirConexion();
+            string sql = @"SELECT `id_inquilino`, `dni`, `nombre`, `apellido`, `contacto` 
+                        FROM `inquilino` WHERE `id_inquilino`=@id";
+
+            MySqlCommand comando = new MySqlCommand(sql, this.conexionsql);
+            comando.Parameters.AddWithValue("@id", id);
+            comando.CommandType = CommandType.Text;
+            var lector = comando.ExecuteReader();
+
+            InquilinoModel i = new InquilinoModel();
+            if (lector.Read())
+            {
+                i.Id_inquilino = lector.GetInt16("id_inquilino");
+                i.Dni = lector.GetString("dni");
+                i.Nombre = lector.GetString("nombre");
+                i.Apellido = lector.GetString("apellido");
+                i.Contacto = lector.GetString("contacto");
+            }
+            else
+            { i.Id_inquilino = -1; }
+            this.cerrarConexion();
+
+            return i;
+        }
+    }
+}
