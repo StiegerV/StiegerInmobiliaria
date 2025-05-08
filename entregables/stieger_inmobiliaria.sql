1ca4c282b35920b88f1d116c5ff8016dfc2a7fcf
Valentin Stieger <50178370+StiegerV@users.noreply.github.com>
2025-05-08 19:53:32 -0300

    actualizacion bd

--- entregables/stieger_inmobiliaria.sql
+++ entregables/stieger_inmobiliaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2025 at 02:55 AM
+-- Generation Time: May 09, 2025 at 12:52 AM
 -- Server version: 5.7.17
 -- PHP Version: 5.6.30
 
@@ -35,6 +35,7 @@ CREATE TABLE `contrato` (
   `fecha_fin` date NOT NULL,
   `id_inmueble` int(11) NOT NULL,
   `id_inquilino` int(11) NOT NULL,
+  `fecha_fin_original` datetime DEFAULT NULL,
   `creado_por` int(11) NOT NULL,
   `terminado_por` int(11) DEFAULT NULL,
   `activo` tinyint(1) NOT NULL DEFAULT '1'
@@ -44,12 +45,9 @@ CREATE TABLE `contrato` (
 -- Dumping data for table `contrato`
 --
 
-INSERT INTO `contrato` (`id_contrato`, `monto`, `fecha_inicio`, `fecha_fin`, `id_inmueble`, `id_inquilino`, `creado_por`, `terminado_por`, `activo`) VALUES
-(1, 10, '2025-04-03', '2025-04-03', 1, 2, 1, NULL, 1),
-(7, 1000, '2025-05-02', '2025-05-04', 2, 6, 1, NULL, 0),
-(6, 1000, '2025-04-01', '2025-04-30', 1, 6, 1, 1, 1),
-(8, 1000, '2025-05-02', '2025-05-03', 1, 6, 1, NULL, 1),
-(9, 1000, '2025-05-03', '2025-10-04', 2, 8, 1, NULL, 1);
+INSERT INTO `contrato` (`id_contrato`, `monto`, `fecha_inicio`, `fecha_fin`, `id_inmueble`, `id_inquilino`, `fecha_fin_original`, `creado_por`, `terminado_por`, `activo`) VALUES
+(12, 10, '2025-05-17', '2025-06-17', 2, 5, '2025-06-17 00:00:00', 1, NULL, 1),
+(13, 80085, '2025-05-08', '2025-12-08', 7, 5, '2025-12-08 00:00:00', 1, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -76,9 +74,11 @@ CREATE TABLE `inmueble` (
 --
 
 INSERT INTO `inmueble` (`id_inmueble`, `id_propietario`, `direccion`, `uso`, `tipo`, `ambientes`, `cordenadas`, `precio`, `disponible`, `activo`, `imagen`) VALUES
-(1, 4, 'calle siemprevivatest edit', 'residencial', 'casa', 12, '50', 10, 'suspendido', 1, '/uploads/638810116914225870.png'),
-(2, 4, 'direccionPrueba', 'comercial', 'local', 1, '1;1', 10.15, 'disponible', 1, ''),
-(3, 6, 'testcrearinmueble', 'otro', 'departamento', 80085, '080085', 80085, 'disponible', 1, '');
+(1, 4, 'calle siemprevivatest edit', 'residencial', 'casa', 12, '50', 10, 'suspendido', 1, NULL),
+(2, 4, 'direccionPrueba', 'comercial', 'local', 1, '1;1', 10.15, 'disponible', 1, NULL),
+(3, 6, 'testcrearinmueble', 'otro', 'departamento', 80085, '080085', 80085, 'disponible', 1, '/uploads/638823169200886744.png'),
+(7, 13, 'Literally a mountain', 'otro', 'otro', 0, '38;-80', 80085, 'disponible', 1, '/uploads/638823191926381278.png'),
+(6, 13, 'Test imagen', 'residencial', 'departamento', 89, '78;94', 80085, 'disponible', 1, '/uploads/638823186587446053.png');
 
 -- --------------------------------------------------------
 
@@ -107,7 +107,8 @@ INSERT INTO `inquilino` (`id_inquilino`, `dni`, `nombre`, `apellido`, `telefono`
 (5, '44359378', 'pablo', 'power', NULL, NULL, 1),
 (6, '146984321', 'pablito', 'weak', NULL, NULL, 1),
 (7, '16541321', 'PEPITO', 'TOUCHDOWN', '498433', 'yeahbaby@yeah.com', 1),
-(8, '4964323', 'Roberto', 'Estropajo', '1651496843', 'robertito@mail.com', 1);
+(8, '4964323', 'Roberto', 'Estropajo', '1651496843', 'robertito@mail.com', 1),
+(9, '987431369', 'Jhon', 'Denver', '346831684321354', 'westVirginia@mail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -123,6 +124,19 @@ CREATE TABLE `pago` (
   `observacion` varchar(250) DEFAULT NULL,
   `estado` enum('completado','en proceso','fallo','anulado') DEFAULT 'en proceso'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pago`
+--
+
+INSERT INTO `pago` (`id_pago`, `id_contrato`, `monto`, `fecha`, `observacion`, `estado`) VALUES
+(9, 13, 80085, '2025-06-08', 'Pago 6', 'en proceso'),
+(10, 13, 80085, '2025-07-08', 'Pago 7', 'en proceso'),
+(8, 13, 80085, '2025-05-08', 'Pago 5', 'en proceso'),
+(11, 13, 80085, '2025-08-08', 'Pago 8', 'en proceso'),
+(12, 13, 80085, '2025-09-08', 'Pago 9', 'en proceso'),
+(13, 13, 80085, '2025-10-08', 'Pago 10', 'en proceso'),
+(14, 13, 80085, '2025-11-08', 'Pago 11', 'en proceso');
 
 -- --------------------------------------------------------
 
@@ -228,22 +242,22 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `contrato`
 --
 ALTER TABLE `contrato`
-  MODIFY `id_contrato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_contrato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `inmueble`
 --
 ALTER TABLE `inmueble`
-  MODIFY `id_inmueble` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_inmueble` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `inquilino`
 --
 ALTER TABLE `inquilino`
-  MODIFY `id_inquilino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_inquilino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `pago`
 --
 ALTER TABLE `pago`
-  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `propietario`
 --
